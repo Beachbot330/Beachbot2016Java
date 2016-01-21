9355d4b6116fb88fe295799b7958571994c5750a
Allen Peters <git@allenpeters.com>
2016-01-20 20:58:55 -0800

    Implement other climber commands

--- src/org/usfirst/frc330/commands/ClimberPTO_disengage.java
+++ src/org/usfirst/frc330/commands/ClimberPTO_disengage.java
@@ -38,6 +38,7 @@ public class ClimberPTO_disengage extends BBCommand {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	Robot.climber.disengagePTO();
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -46,7 +47,7 @@ public class ClimberPTO_disengage extends BBCommand {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
