2e2bf451c247b54a3440d1bbc4334c737d997862
Joe Ross <git@rossesmail.com>
2016-02-14 12:17:20 -0800

    Make TankDrive command drive

--- src/org/usfirst/frc330/commands/TankDrive.java
+++ src/org/usfirst/frc330/commands/TankDrive.java
@@ -42,6 +42,7 @@ public class TankDrive extends BBCommand {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.chassis.tankDrive(Robot.oi.getDriverL(), Robot.oi.getDriverR());
     }
 
     // Make this return true when this Command no longer needs to run execute()
