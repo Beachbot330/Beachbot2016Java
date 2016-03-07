f66b4b8f90cff0a485dc326e190875334728f4b9
Joe Ross <git@rossesmail.com>
2016-03-06 22:30:50 -0800

    call stop drive in TankDrive init so that PID commands stop

--- src/org/usfirst/frc330/commands/TankDrive.java
+++ src/org/usfirst/frc330/commands/TankDrive.java
@@ -38,6 +38,7 @@ public class TankDrive extends BBCommand {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	Robot.chassis.stopDrive();
     }
 
     // Called repeatedly when this Command is scheduled to run
