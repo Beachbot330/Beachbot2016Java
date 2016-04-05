91171bfb09aa8f637c1dfa138303e0e30ffbdff3
Allen Peters <git@allenpeters.com>
2016-04-04 20:49:28 -0700

    Start driving in auto before arm is done

--- src/org/usfirst/frc330/commands/autocommands/LowBarShoot_waypoint_high.java
+++ src/org/usfirst/frc330/commands/autocommands/LowBarShoot_waypoint_high.java
@@ -16,20 +16,29 @@ import edu.wpi.first.wpilibj.command.BBCommandGroup;
  */
 public class LowBarShoot_waypoint_high extends BBCommandGroup {
     
+	double distanceToLowbar = 35.0;
 	double lowBarY = 125;
 	double initialDriveY = 194;
 	double batterX = 98;
-	double batterY = 273;
+	double batterY = 270;
+	
+	BBCommand armCommand;
 	
 	public static final PIDGains GyroDriveHigh = new PIDGains(0.005,0,0.001,0,1,1, "GyroDriveHigh"); //AP 3-18
 	public static final PIDGains DriveHigh     = new PIDGains(0.050,0,0.10,0,ChassisConst.defaultMaxOutput,ChassisConst.defaultMaxOutputStep, "DriveHigh");
 	
     public  LowBarShoot_waypoint_high() {
     	addSequential(new ShiftLow());
-    	addSequential(new SetTurretPosition(TurretConst.center, 3.0, 20.0));  //angle, tol, timeout
+    	addParallel(new SetTurretPosition(TurretConst.center, 3.0, 20.0));  //angle, tol, timeout
     	addParallel(new PickupOn());
-    	addSequential(new SetArmPosition(ArmConst.lowBar, 3.0, 20.0));  //angle, tol, timeout
-    	addSequential(new DriveDistanceAtAbsAngle(lowBarY, 5.0, 0, 3.0, false, ChassisConst.DriveLow, ChassisConst.GyroDriveLow, ChassisConst.GyroTurnLow) ); 
+    	armCommand = new SetArmPosition(ArmConst.lowBar, 5.0, 20.0); //angle, tol, timeout
+    	addParallel(armCommand);
+    	addSequential(new Wait(1.0));
+    	addParallel(new DriveWaypoint(0.0, distanceToLowbar, 3.0, 5.0, false, DriveHigh, GyroDriveHigh));
+    	
+    	addSequential(new CheckDone(armCommand));
+    	
+    	addSequential(new DriveWaypoint(0.0, lowBarY, 5.0, 3.0, false, ChassisConst.DriveLow, ChassisConst.GyroDriveLow) ); 
     	addSequential(new ShiftHigh());
     	addParallel(new SetArmPosition(ArmConst.shootAngleFloor, 5, 1));
     	addSequential(new DriveWaypoint(3.0, initialDriveY, 5.0, 5.0, false, DriveHigh, GyroDriveHigh));
@@ -44,11 +53,11 @@ public class LowBarShoot_waypoint_high extends BBCommandGroup {
         addSequential(new ShiftHigh());
         addSequential(new DriveWaypoint(batterX,batterY,3,4,false,DriveHigh,GyroDriveHigh));
         //double x, double y, double tolerance, double timeout, boolean stopAtEnd, PIDGains driveGains, PIDGains gyroGains
-        addSequential(new Wait(0.5));
+        addSequential(new Wait(0.7));
         addSequential(new Aim(3.5, 15.0));
         addSequential(new Shoot());
         
-        addSequential(new Wait(0.5));
+        addSequential(new Wait(0.1));
         
         BBCommand driveCommand = new DriveWaypointBackward(0, initialDriveY, 5, 5, false, ChassisConst.GyroDriveHigh, ChassisConst.GyroTurnHigh);
         addParallel(driveCommand);
