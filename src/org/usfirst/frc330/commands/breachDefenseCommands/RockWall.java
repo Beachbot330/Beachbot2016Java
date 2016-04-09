d3fff319cd9f942734f52ad3d7de557f246da681
Allen Peters <git@allenpeters.com>
2016-04-09 12:19:54 -0700

    Working Rock Wall, position 5

--- src/org/usfirst/frc330/commands/breachDefenseCommands/RockWall.java
+++ src/org/usfirst/frc330/commands/breachDefenseCommands/RockWall.java
@@ -1,15 +1,11 @@
 
 package org.usfirst.frc330.commands.breachDefenseCommands;
-import org.usfirst.frc330.commands.PickupOff;
-import org.usfirst.frc330.commands.PickupOn;
-import org.usfirst.frc330.commands.SetArmPosition;
-import org.usfirst.frc330.commands.SetTurretPosition;
-import org.usfirst.frc330.commands.SetXYOffset;
-import org.usfirst.frc330.commands.ShiftLow;
-import org.usfirst.frc330.commands.drivecommands.DriveDistance;
+import org.usfirst.frc330.commands.*;
+import org.usfirst.frc330.commands.drivecommands.*;
 import org.usfirst.frc330.constants.ArmConst;
 import org.usfirst.frc330.constants.ChassisConst;
 import org.usfirst.frc330.constants.TurretConst;
+import org.usfirst.frc330.wpilibj.PIDGains;
 
 import edu.wpi.first.wpilibj.command.BBCommandGroup;
 
@@ -18,14 +14,40 @@ import edu.wpi.first.wpilibj.command.BBCommandGroup;
  */
 public class RockWall extends BBCommandGroup {
     
+	double carefulDriving = 0.4;
+	PIDGains driveCareful	   = new PIDGains(0.100,0,0.000,0,carefulDriving,carefulDriving, "DriveLow");
+	PIDGains gyroDriveLow  = new PIDGains(0.010,0,0.000,0,1,1, "GyroDriveLow");
+	
+	double superCarefulDriving = 0.35;
+	PIDGains creepForward	   = new PIDGains(0.080,0,0.000,0,superCarefulDriving,superCarefulDriving, "DriveLow");
+	
+	double centerOfWallY = 82;
+	double finishPositionY = 176.0;
+	
     public  RockWall() {
+    	addParallel(new SetTurretPosition(TurretConst.center, 3.0, 20.0));
     	addSequential(new ShiftLow());
-    	addSequential(new SetTurretPosition(TurretConst.center, 3.0, 1.0));			    //angle, tol, timeout
-    	addSequential(new SetArmPosition(ArmConst.defaultNeutral, 3.0, 1.0));   				//angle, tol, timeout
-    	addSequential(new DriveDistance(164.0, 5.0, 3.0, false, ChassisConst.DriveLow) ); // distance, tol, timeout, stop
+    	addParallel(new SetArmPosition(ArmConst.defaultNeutral, 5.0, 5.0));
     	addParallel(new PickupOn());
-    	addSequential(new SetArmPosition(ArmConst.shootAngleFloor, 5, 1));
+    	addSequential(new DriveWaypoint(0.0, centerOfWallY-36, 10.0, 15.0, false, ChassisConst.DriveLow, gyroDriveLow));
+    	addSequential(new DriveWaypoint(0.0, centerOfWallY, 2.0, 15.0, false, driveCareful, gyroDriveLow));    	
+    	addParallel(new SetArmPosition(ArmConst.defenseStance, 5.0, 20.0));
+    	addSequential(new Wait(0.1));
+    	
+    	addSequential(new DriveWaypoint(0.0, centerOfWallY+36, 2.0, 15.0, false, driveCareful, gyroDriveLow));
+    	
+    	addParallel(new SetArmPosition(ArmConst.shootAngleFloor, 5.0, 20.0));    	
+    	addParallel(new DriveWaypoint(0.0, finishPositionY, 10.0, 15.0, true, ChassisConst.DriveLow, gyroDriveLow));
     	addSequential(new PickupOff());
-    	addSequential(new SetXYOffset(0,-36));
     }
 }
+
+
+//addSequential(new ShiftLow());
+//addSequential(new SetTurretPosition(TurretConst.center, 3.0, 1.0));			    //angle, tol, timeout
+//addSequential(new SetArmPosition(ArmConst.defaultNeutral, 3.0, 1.0));   				//angle, tol, timeout
+//addSequential(new DriveDistance(164.0, 5.0, 3.0, false, ChassisConst.DriveLow) ); // distance, tol, timeout, stop
+//addParallel(new PickupOn());
+//addSequential(new SetArmPosition(ArmConst.shootAngleFloor, 5, 1));
+//addSequential(new PickupOff());
+//addSequential(new SetXYOffset(0,-36));
--- src/org/usfirst/frc330/commands/driveAndShoot/PositionFive.java
+++ src/org/usfirst/frc330/commands/driveAndShoot/PositionFive.java
@@ -1,13 +1,16 @@
 
 package org.usfirst.frc330.commands.driveAndShoot;
-import org.usfirst.frc330.commands.Aim;
-import org.usfirst.frc330.commands.Wait;
+import org.usfirst.frc330.commands.*;
 import org.usfirst.frc330.commands.commandgroups.Shoot;
 import org.usfirst.frc330.commands.drivecommands.DriveTime;
 import org.usfirst.frc330.commands.drivecommands.DriveWaypoint;
+import org.usfirst.frc330.commands.drivecommands.DriveWaypointBackward;
 import org.usfirst.frc330.commands.drivecommands.TurnGyroWaypoint;
+import org.usfirst.frc330.constants.ArmConst;
 import org.usfirst.frc330.constants.ChassisConst;
+import org.usfirst.frc330.constants.TurretConst;
 
+import edu.wpi.first.wpilibj.command.BBCommand;
 import edu.wpi.first.wpilibj.command.BBCommandGroup;
 
 /**
@@ -15,15 +18,38 @@ import edu.wpi.first.wpilibj.command.BBCommandGroup;
  */
 public class PositionFive extends BBCommandGroup {
     
+	double pivotPointX = 0;
+	double startY = 176;
+	double pivotPointY = 288-35;
+	
+	double batterX = -37;
+	double batterY = 290;
+	
     public  PositionFive() {
-    	addSequential(new DriveWaypoint(0, 288-37, 5, 5, true, ChassisConst.DriveLow, ChassisConst.GyroDriveLow));
-        addSequential(new TurnGyroWaypoint(-37, 288, 5, 3, ChassisConst.GyroTurnLow));
+    	addSequential(new DriveWaypoint(pivotPointX, pivotPointY, 5, 5, true, ChassisConst.DriveLow, ChassisConst.GyroDriveLow));
+        addSequential(new TurnGyroWaypoint(batterX, batterY, 8, 2, ChassisConst.GyroTurnLow));
         //double x, double y, double tolerance, double timeout, PIDGains gains
-        addSequential(new DriveWaypoint(-37, 288, 5, 5, true, ChassisConst.DriveLow, ChassisConst.GyroDriveLow));
+        addSequential(new DriveWaypoint(batterX, batterY, 5, 5, true, ChassisConst.DriveLow, ChassisConst.GyroDriveLow));
         //double x, double y, double tolerance, double timeout, boolean stopAtEnd, PIDGains driveGains, PIDGains gyroGains
         addParallel(new DriveTime(5.0,0.1,0.1));
         addSequential(new Aim(3.5, 15.0));
         addSequential(new Shoot());
         addSequential(new Wait(0.2));
+        
+        addSequential(new ShiftHigh());
+        BBCommand driveCommand = new DriveWaypointBackward(pivotPointX, pivotPointY, 10, 5, false, ChassisConst.GyroDriveHigh, ChassisConst.GyroTurnHigh);
+        addParallel(driveCommand);
+        //double x, double y, double tolerance, double timeout, boolean stopAtEnd, PIDGains driveGains, PIDGains gyroGains
+        addSequential(new Wait(0.5));
+        addParallel(new SetTurretPosition(TurretConst.center, 3.0, 20.0));  //angle, tol, timeout
+        addParallel(new SetArmPosition(ArmConst.defenseStance, 3.0, 20.0));  //angle, tol, timeout
+        
+        addSequential(new CheckDone(driveCommand));
+        
+        addSequential(new ShiftLow());
+        addSequential(new TurnGyroWaypoint(0, startY, 5, 2, ChassisConst.GyroTurnLow));
+        
+        addSequential(new ShiftHigh());
+        addSequential(new DriveWaypoint(0, startY, 10, 5, true, ChassisConst.GyroDriveHigh, ChassisConst.GyroTurnHigh));
     }
 }
