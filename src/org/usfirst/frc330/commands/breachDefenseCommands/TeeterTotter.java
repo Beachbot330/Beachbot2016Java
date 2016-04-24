0521f571fed235facb4d87dde9e54c2b51152200
Joe Ross <git@rossesmail.com>
2016-04-24 15:36:37 -0700

    Offset Cheval auto by 4 inches right to account for where we line up

--- src/org/usfirst/frc330/commands/breachDefenseCommands/TeeterTotter.java
+++ src/org/usfirst/frc330/commands/breachDefenseCommands/TeeterTotter.java
@@ -24,24 +24,26 @@ public class TeeterTotter extends BBCommandGroup {
 	double onRamps = distanceToTeeterY+35;
 	double tippingPoint = distanceToTeeterY+13+30;
 	double finishPositionY = 160.0;
+	double xOffset = 4.0;
 	
 	
     public  TeeterTotter() {
+    	addSequential(new SetXYOffset(xOffset,0.0));
     	addParallel(new SetTurretPosition(TurretConst.center, 3.0, 20.0));
     	addSequential(new ShiftLow());
     	addParallel(new PickupOn());
-    	addSequential(new DriveWaypoint(0.0, distanceToTeeterY, 2.0, 15.0, false, driveLow, gyroDriveLow));
+    	addSequential(new DriveWaypoint(xOffset, distanceToTeeterY, 2.0, 15.0, false, driveLow, gyroDriveLow));
     	addSequential(new SetArmPosition(ArmConst.lowBar, 5.0, 20.0));
     	
-    	addSequential(new DriveWaypoint(0.0, onRamps, 2.0, 15.0, false, driveLow, gyroDriveLow));
+    	addSequential(new DriveWaypoint(xOffset, onRamps, 2.0, 15.0, false, driveLow, gyroDriveLow));
     	
     	addParallel(new SetArmPosition(ArmConst.defaultNeutral, 5.0, 20.0));
-    	addSequential(new DriveWaypoint(0.0, tippingPoint, 2.0, 15.0, true, creepForward, gyroDriveLow));
+    	addSequential(new DriveWaypoint(xOffset, tippingPoint, 2.0, 15.0, true, creepForward, gyroDriveLow));
     	addSequential(new Wait(0.7)); //time to tip
     	
-    	addParallel(new SetArmPosition(ArmConst.shootAngleFloor, 5, 1));
+    	addParallel(new SetArmPosition(ArmConst.defaultNeutral, 5, 1));
     	//addSequential(new DriveWaypoint(0.0, distanceToTeeterY+13+29.5+5, 4.0, 15.0, false, driveLow, gyroDriveLow));
     	addSequential(new ShiftHigh());
-    	addSequential(new DriveWaypoint(0.0, finishPositionY, 4.0, 15.0, false, ChassisConst.DriveHigh, ChassisConst.GyroDriveHigh));
+    	addSequential(new DriveWaypoint(xOffset, finishPositionY, 4.0, 15.0, false, ChassisConst.DriveHigh, ChassisConst.GyroDriveHigh));
     }
 }
